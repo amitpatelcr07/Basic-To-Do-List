24b35ef7297d78a5e7a81e1892366e2034321cc0
amitpatelcr07 <amitcoder07@gmail.com>
2024-01-16 23:14:38 +0530

    Js code chages for adding new list item tag via Js

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-let input=document.querySelector(".in")
-let add=document.querySelector(".add");
+let button=document.querySelector(".add");
+let input=document.querySelector(".search");
+
+let Add;
+let li;
+
+    Add=()=>{
+    //    input.value==''?alert("enter a value"): console.log("amit");
+       
+    if(input.value==''){
+             alert("enter a value")
+    }else{
+        li=document.createElement('li');
+   
+        li.innerHTML=`${input.value}`;
+        li.className="abc";
+        li.style.textAlign="center"
+        li.style.marginTop="20px"
+        li.style.borderRadius="10px"
+        li.style.backgroundColor="red"
+        document.querySelector(".field").appendChild(li);
+        input.value=''
+    }
+    
+    }
 
 
-let arrow=()=>{
-    let p=document.createElement("p");
-
-    console.log("arrow work");
-    console.log(input.value);
-     p.innerHTML=`<p>${input.value}</p>`;
-     p.style.border = "2px solid red";
-     p.style.textAlign="center"
-     document.querySelector('.parent').appendChild(p)
-
-}
-
-add.addEventListener("click",arrow);
+button.addEventListener("click",Add);
